23b293d6b63e33cf2fe9014f9800f756dec4cc4f
S1mpleNickname <81587847+S1mpleNickname@users.noreply.github.com>
2021-07-02 13:53:21 +0300

    Create style.css

---
+++ style.css
@@ -0,0 +1,321 @@
+img {
+    width: 175px;
+    margin: 15px;
+}
+html {
+    font-family: sans-serif;
+    display: inline-block;
+}
+a {
+    text-decoration: none;
+    color: black;
+    margin: 10px;
+    margin-left: 48px;
+}
+.hh {
+    box-shadow: 0 0 5px;
+    padding: 30px;
+    position: relative;
+    margin-left: 10px;
+    width: 1235px;
+}
+a:hover {
+    color: #8E58D3;
+    font-weight: bold;
+}
+.icons {
+    width: 15px;
+}
+.a::before {
+    display: block;
+    width: 200px;
+    height: 10px;
+    opacity: 0;
+}
+.a:hover::before {
+    content: url("https://learn.algoritmika.org/uploads/student/740714/14-1625064305.png");
+    display: inline;
+    opacity: 1;
+}
+.b::before {
+    display: block;
+    width: 200px;
+    height: 10px;
+    opacity: 0;
+}
+.b:hover::before {
+    content: url("https://learn.algoritmika.org/uploads/student/740714/15-1625064791.png");
+    display: inline;
+    opacity: 1;
+}
+.c::before {
+    display: block;
+    width: 200px;
+    height: 10px;
+    opacity: 0;
+}
+.c:hover::before {
+    content: url("https://learn.algoritmika.org/uploads/student/740714/16-1625065325.png");
+    display: inline;
+    opacity: 1;
+}
+.d::before {
+    display: block;
+    width: 200px;
+    height: 10px;
+    opacity: 0;
+}
+.d:hover::before {
+    content: url("https://learn.algoritmika.org/uploads/student/740714/17-1625065530.png");
+    display: inline;
+    opacity: 1;
+}
+.e::before {
+    display: block;
+    width: 200px;
+    height: 10px;
+    opacity: 0;
+}
+.e:hover::before {
+    content: url("https://learn.algoritmika.org/uploads/student/740714/18-1625065641.png");
+    display: inline;
+    opacity: 1;
+}
+.f::before {
+    display: block;
+    width: 200px;
+    height: 10px;
+    opacity: 0;
+}
+.f:hover::before {
+    content: url("https://learn.algoritmika.org/uploads/student/740714/19-1625065746.png");
+    display: inline;
+    opacity: 1;
+}
+.g::before {
+    display: block;
+    width: 200px;
+    height: 10px;
+    opacity: 0;
+}
+.g:hover::before {
+    content: url("https://learn.algoritmika.org/uploads/student/740714/21-1625065975.png");
+    display: inline;
+    opacity: 1;
+}
+.h::before {
+    display: block;
+    width: 200px;
+    height: 10px;
+    opacity: 0;
+}
+.h:hover::before {
+    content: url("https://learn.algoritmika.org/uploads/student/740714/22-1625066079.png");
+    display: inline;
+    opacity: 1;
+}
+.i::before {
+    display: block;
+    width: 200px;
+    height: 10px;
+    opacity: 0;
+}
+.i:hover::before {
+    content: url("https://learn.algoritmika.org/uploads/student/740714/23-1625066185.png");
+    display: inline;
+    opacity: 1;
+}
+.j::before {
+    display: block;
+    width: 200px;
+    height: 10px;
+    opacity: 0;
+}
+.j:hover::before {
+    content: url("https://learn.algoritmika.org/uploads/student/740714/24-1625066303.png");
+    display: inline;
+    opacity: 1;
+}
+.hl {
+    padding: 15px;
+    margin: 15px;
+
+}
+.cool {
+    margin: 15px;
+    color: grey;
+}
+.divik2 {
+    width: 1000%;
+}
+.divik3 {
+    height: 325px;
+    margin: 15px;
+    width: 49%px;
+    box-shadow: 0 0 5px;
+    display: inline-block;
+    float: left;
+    width: 300px;
+}
+.divik4 {
+    height: 325px;
+    margin: 15px;
+    width: 49%px;
+    box-shadow: 0 0 5px;
+    display: inline-block;
+    float: left;
+    width: 300px;
+}
+.cool2 {
+    margin: 15px;
+}
+.h2 {
+    margin: 15px;
+}
+.divik5 {
+    height: 325px;
+    margin: 15px;
+    width: 49%px;
+    box-shadow: 0 0 5px;
+    display: inline-block;
+    float: left;
+    width: 300px;
+}
+.divik6 {
+    height: 325px;
+    margin: 15px;
+    width: 49%px;
+    box-shadow: 0 0 5px;
+    display: inline-block;
+    float: left;
+    width: 300px;
+}
+.yt {
+    position: relative;
+}
+.bb {
+    position: absolute;
+    margin-top: 400px;
+    box-shadow: 0 0 5px;
+    margin-left: 15px;
+    width: 300px;
+}
+.uy {
+    position: relative;
+}
+.yu {
+    position: absolute;
+    margin-top: 400px;
+    box-shadow: 0 0 5px;
+    margin-left: 345px;
+    width: 300px;
+}
+.po {
+    position: relative;
+}
+.op {
+    position: absolute;
+    margin-top: 400px;
+    box-shadow: 0 0 5px;
+    margin-left: 675px;
+    width: 300px;
+}
+.nm {
+    position: relative;
+}
+.mn {
+    position: absolute;
+    margin-top: 400px;
+    box-shadow: 0 0 5px;
+    margin-left: 1005px;
+    width: 300px;
+}
+.jh {
+    position: relative;
+}
+.hj {
+    position: absolute;
+    margin-top: 770px;
+    box-shadow: 0 0 5px;
+    margin-left: 15px;
+    width: 300px;
+}
+.lk {
+    position: relative;
+}
+.kl {
+    position: absolute;
+    margin-top: 770px;
+    box-shadow: 0 0 5px;
+    margin-left: 345px;
+    width: 300px;
+}
+.rt {
+    position: relative;
+}
+.tr {
+    position: absolute;
+    margin-top: 770px;
+    box-shadow: 0 0 5px;
+    margin-left: 675px;
+    width: 300px;
+
+}
+.fg {
+    position: relative;
+}
+.gf {
+    position: absolute;
+    margin-top: 770px;
+    box-shadow: 0 0 5px;
+    margin-left: 1005px;
+    width: 300px;
+}
+.oi {
+    position: relative;
+}
+.io {
+    position: absolute;
+    margin-top: 1140px;
+    box-shadow: 0 0 5px;
+    margin-left: 15px;
+    width: 300px;
+}
+.bn {
+    position: relative;
+}
+.nb {
+    position: absolute;
+    margin-top: 1140px;
+    box-shadow: 0 0 5px;
+    margin-left: 345px;
+    width: 300px;
+}
+.we {
+    position: relative;
+}
+.ew {
+    position: absolute;
+    margin-top: 1140px;
+    box-shadow: 0 0 5px;
+    margin-left: 675px;
+    width: 300px;
+}
+.aw {
+    position: relative;
+}
+.wa {
+    position: absolute;
+    margin-top: 1140px;
+    box-shadow: 0 0 5px;
+    margin-left: 1005px;
+    width: 300px;
+}
+.krutoi {
+    width: 50px;
+    height: 50px;
+}
+.loh {
+    margin-top: 1130px;
+    margin-left: 15px;
+}
